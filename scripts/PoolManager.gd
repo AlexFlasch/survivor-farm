83252096d1acd5c2831ef2600d3b5a53ce3dec4e
Brandon Tom <brandon@geocaching.com>
2025-04-01 09:56:35 -0700

    feat: add instance retrieval and return functionality to PoolManager

--- scripts/PoolManager.gd
+++ scripts/PoolManager.gd
@@ -15,6 +15,29 @@ func register_pool(pool_name: String, pool_size: int, scene_path: PackedScene) -
 		add_child(instance)
 	pools[pool_name] = pool
 	scenes[pool_name] = scene_path
+	
+func get_from_pool(pool_name: String) -> Node:
+	if pool_name not in pools:
+		print("Pool not found: ", pool_name)
+		return null
+	var pool: Array = pools[pool_name]
+	for instance in pool:
+		if not instance.visible:
+			instance.visible = true
+			return instance
+	print("No available instances in pool: ", pool_name)
+	return null
+
+func return_to_pool(instance: Node) -> void:
+	if instance == null:
+		print("Instance is null, cannot return to pool.")
+		return
+	for pool_name in pools.keys():
+		var pool: Array = pools[pool_name]
+		if instance in pool:
+			instance.visible = false
+			return
+	print("Instance not found in any pool: ", instance)
 
 func _ready() -> void:
 	pass
