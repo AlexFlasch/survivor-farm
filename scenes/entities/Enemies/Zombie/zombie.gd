3702ad3a7be3bb60ae18bed522c349d34f1cf052
Brandon Tom <magicink@gmail.com>
2025-04-02 09:21:53 -0700

    feat: add stunned state Zombie enemy

--- scenes/entities/Enemies/Zombie/zombie.gd
+++ scenes/entities/Enemies/Zombie/zombie.gd
@@ -2,12 +2,26 @@ extends CharacterBody2D
 
 @export var follow_speed: float = 150.0
 @export var stop_distance: float = 10.0
+@export var bounce_distance: float = 20.0  # New variable for bounce distance
+
+# New variables for stunned state
+var is_stunned: bool = false
+var stun_duration: float = 1.0  # seconds
+var stun_time_left: float = 0.0
+
 var player: Node2D
 
 func _ready() -> void:
 	player = get_parent().get_node("Player")
 
 func _process(delta: float) -> void:
+	# Skip movement if stunned
+	if is_stunned:
+		stun_time_left -= delta
+		if stun_time_left <= 0:
+			is_stunned = false
+		return
+	
 	if not get_tree().root.get_node("GameManager").is_game_active():
 		return
 	
