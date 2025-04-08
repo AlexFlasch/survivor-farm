3d1424d6d3851b309fe2ff6d83dc8647afce6e4b
AlexFlasch <alexanderflasch@gmail.com>
2025-04-07 20:53:28 -0700

    fix: integer division caused health bar to be only full or empty

--- scenes/ui/ExpBar/exp_bar.gd
+++ scenes/ui/ExpBar/exp_bar.gd
@@ -3,31 +3,20 @@ class_name ExpBar extends NinePatchRect
 @export var show_value: bool = true
 @export var current_value: int = 0
 @export var max_value: int = 100
-@export var percentage: float = 0.0
 @export var bar_material: ShaderMaterial = preload("res://resources/materials/ProgressBar.material") as ShaderMaterial
 
 @onready var progress_rect :TextureRect = %ProgressBar
 @onready var value_label :Label = %ValueLabel
-@onready var gm: Node = get_tree().root.get_node("GameManager")
 
 func _ready() -> void:
-	if not gm == null:
-		current_value = gm.get_player_health()
-		gm.connect("health_changed", Callable(self, "_on_health_changed"))
-		gm.connect("max_health_changed", Callable(self, "_on_max_health_changed"))
 	if not show_value:
 		value_label.hide()
 
 func _process(delta: float) -> void:
 	if progress_rect != null:
+		var percentage = float(current_value) / float(max_value)
 		progress_rect.material = bar_material
 		progress_rect.material.set('shader_parameter/progress', percentage)
 	
 	if value_label != null:
 		value_label.text = '%s/%s' % [current_value, max_value]
-
-func _on_health_changed(health: int) -> void:
-	current_value = health
-
-func _on_max_health_changed(new_max: int) -> void:
-	max_value = new_max
--- scenes/ui/PlayerHud/player_hud.gd
+++ scenes/ui/PlayerHud/player_hud.gd
@@ -1,19 +1,21 @@
 class_name PlayerHud extends PanelContainer
 
-@export var player_health_percentage: float = 100.0
 @export var player_level: int = 1
 @export var player_exp_percentage: float = 100.0
 
 @onready var health_bar: ExpBar = %HealthBar
 @onready var level_label: Label = %PlayerLevel
 @onready var exp_bar: ExpBar = %ExpBar
+@onready var gm: Node = get_tree().root.get_node("GameManager")
+
+func _ready() -> void:
+	if not gm == null:
+		var player_health = gm.get_player_health()
 
 func _process(delta: float) -> void:
 	if health_bar != null:
-		health_bar.percentage = player_health_percentage
+		health_bar.current_value = gm.get_player_health()
+		health_bar.max_value = gm.get_player_max_health()
 	
 	if level_label != null:
 		level_label.text = str(player_level)
-	
-	if exp_bar != null:
-		exp_bar.percentage = player_exp_percentage
