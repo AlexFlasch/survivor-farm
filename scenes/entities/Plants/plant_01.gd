23baf6ebde60f6ce7be561d268cd77029e61dec7
Brandon Tom <magicink@gmail.com>
2025-06-15 14:24:21 -0700

    fix: update timers to respect game's pause state

--- scenes/entities/Plants/plant_01.gd
+++ scenes/entities/Plants/plant_01.gd
@@ -4,7 +4,7 @@ var animated_sprite: AnimatedSprite2D
 
 func _ready():
 	animated_sprite = $AnimatedSprite2D
-	await get_tree().create_timer(10.0).timeout
+	await get_tree().create_timer(10.0, false).timeout
 	animated_sprite.play("sprout")
-	await get_tree().create_timer(20.0).timeout
+	await get_tree().create_timer(20.0, false).timeout
 	animated_sprite.play("mature")
